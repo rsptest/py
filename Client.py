788ad2aac23c3ce91a683940d33013936fc8db73
rsptest <81948058+rsptest@users.noreply.github.com>
2021-04-04 22:12:49 -0700

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,22 @@
+from socket import *
+
+# Running on mininet
+serverName = 'Ren' #Hostname goes here
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+# Receive the message to be the setting clients
+clientName = clientSocket.recv(1024).decode()
+print("From Server "+ clientName)
+
+# Sending message to the server
+msg = input("Enter a message to send to server: ")
+clientSocket.send(msg.encode())
+
+# Receive the final message
+fresponse = clientSocket.recv(1024).decode()
+print("From Server: " + fresponse)
+
+# Close the connection
+clientSocket.close()
